a35dc3a75d8c22cfe3338420c4d8a772510724bb
Ravin Poudel <poudel@ARSMSSGB2A16007.local>
2019-11-06 17:19:26 -0500

    code for strand specificity added

--- predictPAM/main.py
+++ predictPAM/main.py
@@ -30,7 +30,8 @@ def myparser():
     parser.add_argument('--gbkfile', '-i', type=str, required=True,
                         help='A genbank .gbk file')
     parser.add_argument('--pamseq', '-p', type=str, required=True, help='A short PAM motif to search for, may be use IUPAC ambiguous alphabet'),
-    parsr.add_argument('--targetsize', '-s', type=int, default=22, help='Length of the target sequence'),
+    parser.add_argument('--targetlength', '-l', type=int, default=22, help='Length of the target sequence'),
+    parser.add_argument('--strand', '-s', type=str, default="+", help='Strand to DNA'),
     parser.add_argument('--outfile', '-o', type=str, required=True, help='The table of pam sites and data')
     parser.add_argument('--tempdir', help='The temp file directory', default=None)
     parser.add_argument('--keeptemp' ,help="Should intermediate files be kept?", action='store_true')
@@ -39,7 +40,7 @@ def myparser():
     return parser
 
 # GLOBAL parsing variables
-
+ALLOWED_FILE_EXTENSIONS = set(['gbk'])
 
 
 def _logger_setup(logfile):
@@ -69,11 +70,47 @@ def _logger_setup(logfile):
         print("An error occurred setting up logging")
         raise e
 
+####################
+def allowed_file(filename):
+    """ Check for the correct file type
+    
+    Args:
+        filename(file): File to process
+    
+    Returns:
+        Booleans(): True or False
+    
+    """
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_FILE_EXTENSIONS
+    
+    
 
+def get_fastas(genbank, tempdir):
+    """Retruns fasta and complement of fasta for a given genbank file
 
+    Args:
+        genbank (genebank): Genbank file to process
+
+    Returns:
+        (out.fasta): Fasta file in forward orientation (5'-3')
+        (out_complement.fasta): Complement of Fasta file in reverese orientation (3'-5')
+
+    """
+    if allowed_file(genbank):
+        sequences = []
+        sequences_complement=[]
+        for record in SeqIO.parse(genbank, "genbank"):
+            sequences.append(record)
+            sequences_complement.append(SeqRecord(record.seq.complement(), record.id+"_complement",
+            description=record.description+"_complement", name=record.name+"_complement"))
+        SeqIO.write(sequences, os.path.join(tempdir,"out.fasta"), "fasta")
+        SeqIO.write(sequences_complement, os.path.join(tempdir,"out_complement.fasta"), "fasta")
+    else:
+        print("Invalid file type, allowed file type is .gbk")
+        
 
 ############### MAPPING ###########################################
-def map_pam(tempdir, pamseq, threads=1):
+def map_pam(tempdir, pamseq,threads,strand):
     """Runs seqkit locate to find the pam in given genome (FASTA)
 
     Args:
@@ -85,15 +122,27 @@ def map_pam(tempdir, pamseq, threads=1):
     """
     try:
         infasta = os.path.join(tempdir, "out.fasta")
-        parameters = ["seqkit",
-                       "locate",
-                       "-p", pamseq,
-                       infasta,"-P",
-                       "--bed",
-                        "--threads", str(threads)]
-        p = subprocess.Popen(parameters, stdout=subprocess.PIPE)
-        out, err = p.communicate()
-        out = out.decode('utf-8')
+        infasta_complement = os.path.join(tempdir, "out_complement.fasta")
+        if strand == "+":
+            parameters = ["seqkit",
+                           "locate",
+                           "-p", pamseq,
+                           infasta,"-P",
+                           "--bed",
+                            "--threads", str(threads)]
+            p = subprocess.Popen(parameters, stdout=subprocess.PIPE)
+            out, err = p.communicate()
+            out = out.decode('utf-8')
+        if strand =="-":
+            parameters = ["seqkit",
+                           "locate",
+                           "-p", pamseq,
+                           infasta_complement,"-P",
+                           "--bed",
+                            "--threads", str(threads)]
+            p = subprocess.Popen(parameters, stdout=subprocess.PIPE)
+            out, err = p.communicate()
+            out = out.decode('utf-8')
         return out
     except subprocess.CalledProcessError as e:
         raise e
@@ -102,48 +151,66 @@ def map_pam(tempdir, pamseq, threads=1):
 
 
 ######## Retrieving target sequence ################################
-def get_target(tempdir, mappingdata, pamlength, targetsize):
-"""Given bedfile of PAM locations, function goes up or down and finds target
+def get_target(tempdir, mappingdata, targetlength, strand):
+    """Given bedfile of PAM locations, function goes up or down and finds target
 
-Args:
-    tempdir (str): Temporary directory
-    mappingdata (str): Bedfile output from map_pam
+    Args:
+        tempdir (str): Temporary directory
+        mappingdata (str): Bedfile output from map_pam
 
-Returns:
-    (str): Bedfile format with matches
-
-"""
+    Returns:
+        (str): Bedfile format with matches
+    
+    """
     mapping_list = []
     infasta = SeqIO.read(os.path.join(tempdir, "out.fasta"), "fasta")
+    infasta_complement = SeqIO.read(os.path.join(tempdir, "out_complement.fasta"), "fasta")
     bylines = mappingdata.splitlines()
-    for entry in bylines:
-        tline = entry.split()
-        start_ps = int(tline[1])
-        pam_start = start_ps-1
-        cut_ps = start_ps - pamlength
-        target_sp = start_ps - targetsize
-        target_ep = start_ps-2
-        target_seq = str(infasta.seq)[target_sp:target_ep]
-        accession = infasta.id
-        pamseq = tline[3]
-        #if PAM match at the beginning or the end then  target seq might be smaller or none
-        if len(target_seq) == targetsize: # look into why 20 vs 22
-            mapping_list.append({"accession": accession, "target_sp": target_sp,
-            "target_ep": target_ep, "target_seq": target_seq, "pam_seq": pamseq,
-            "cut-pos": cut_ps, "pam-pos": pam_start, "strand": "+"})
-        return mapping_list
+    if strand == "+":
+        for entry in bylines:
+            tline = entry.split()
+            pam_start_ps = int(tline[1]) - 1 # -1 to adjust- beacaue seqkit convention - starts from 1 but in python starts from 0.
+            pam_end_ps = int(tline[2])
+            pam_seq = tline[3]
+            target_sp = pam_start_ps - targetlength
+            target_seq = str(infasta.seq)[target_sp:pam_start_ps]
+            seqid = infasta.id
+            #if PAM match at the beginning or the end then  target seq might be smaller or none
+            if len(target_seq) == targetlength:
+                mapping_list.append({"seqid ": seqid, "target_sp": target_sp,
+                "target_ep": pam_start_ps, "target_seq": target_seq, "pam_seq": pam_seq,
+                "pam_start_ps": pam_start_ps, "strand": "+"})
+    if strand == "-":
+        for entry in bylines:
+            tline = entry.split()
+            pam_start_ps = int(tline[1]) - 1 # -1 to adjust- beacaue seqkit convention - starts from 1 but in python starts from 0.
+            pam_end_ps = int(tline[2])
+            pam_seq = tline[3]
+            target_ep = pam_end_ps + targetlength
+            target_seq = str(infasta_complement.seq)[pam_end_ps:target_ep]
+            seqid = infasta.id
+            #if PAM match at the beginning or the end then  target seq might be smaller or none
+            if len(target_seq) == targetlength:
+                mapping_list.append({"seqid ": seqid, "target_sp": pam_end_ps,
+                "target_ep": target_ep, "target_seq": target_seq, "pam_seq": pam_seq,
+                "pam_start_ps": pam_start_ps, "strand": "-"})
+    return mapping_list
+############### Means to filter target sequences ############
+# tempdir='/var/folders/52/rbrrfj5d369c35kd2xrktf3m0000gq/T/pamPredict_5ii1no_x'
+# test1 = get_target(tempdir, map_data, targetlength=20, strand="+")
+
 
 #############
 def get_cds(genebank_record):
-"""Return a list of CDS's for a genbank file
+    """Return a list of CDS's for a genbank file
 
-Args:
-    genbank_record (obj): Biopython genbank object
+    Args:
+        genbank_record (obj): Biopython genbank object
 
 
-Returns:
-    (list): List of CDS
-"""
+    Returns:
+        (list): List of CDS
+    """
     cds_list = []
     for cds_record in genebank_record:
         for cds_feature in cds_record.features:
@@ -275,37 +342,39 @@ def main(args=None):
             tempdir = tempfile.mkdtemp(prefix='pamPredict_', dir=args.tempdir)
         logging.info("Temp directory is: %s", tempdir)
         # Try to avoid writing out and reading genome in fasta format
-        SeqIO.write(SeqIO.parse(args.gbkfile, "genbank"), os.path.join(tempdir, "out.fasta"), "fasta")
+        logging.info("Retriving fastas- forward and reverse from a genbanke file")
+        get_fastas(genbank=args.gbkfile, tempdir=tempdir)
         print(tempdir)
         # mapping
         logging.info("Mapping pam to the genome")
-        mapfile = map_pam(tempdir=tempdir, pamseq=args.pamseq, threads=args.threads)
+        mapfile = map_pam(tempdir=tempdir, pamseq=args.pamseq, threads=args.threads, strand=args.strand)
 
         #Retrieving target sequence
         logging.info("Retrieving target sequence for matching PAM : %s", tempdir)
-        targetlist = get_target(tempdir=tempdir, mappingfile=mapfile, pamlength=len(args.pamseq), targetsize=args.targetsize)
+        targetlist = get_target(tempdir=tempdir, mappingdata=mapfile, targetlength=args.targetlength, strand=args.strand)
+
 
         # get cds list
         logging.info("Retrieving CDS information")
         cdslist = get_cds(SeqIO.parse(args.gbkfile, "genbank"))
-
+        
         # get gene list
         logging.info("Retrieving Gene information")
         genelist = get_gene(SeqIO.parse(args.gbkfile, "genbank"))
-
+        
         # merge cds and genelist
-        logging.info("Merge cda and gene file : %s", tempdir)
+        logging.info("Merge cda and gene file- features.txt located at : %s", tempdir)
         merge_cds_gene(cdslist=cdslist,genelist=genelist, tempdir=tempdir)
-
+        
         # pybedtools
         logging.info("Mapping features downstream of target sequence")
         find_downstream = pybed_downstream(tempdir,mapfile_from_pam=mapfile)
-
+        
         logging.info("Mapping features upstream of target sequence")
         find_upstream = pybed_upstream(tempdir,mapfile_from_pam=mapfile)
-
+        
         # merge upstream and downstream output
-        logging.info("Writing features file : %s", tempdir)
+        logging.info("Writing features file all.txt - : %s", tempdir)
         merge_downstream_upstream(find_downstream,find_upstream,tempdir)
 
     except Exception as e:
