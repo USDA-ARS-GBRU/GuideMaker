2f5f65dd0186d8d1a82290814d4ce39eddeee5dc
Ravin Poudel <poudel@ARSGAATH5D83RW5.usda.net>
2019-11-12 10:01:10 -0500

    code for review nov 12

--- predictPAM/main.py
+++ predictPAM/main.py
@@ -27,6 +27,7 @@ from itertools import permutations
 from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from pybedtools import BedTool
+from collections import Counter
 
 def myparser():
     parser = argparse.ArgumentParser(description='predictPAM: A python module to predict custom PAM sites in any small genome')
@@ -44,6 +45,8 @@ def myparser():
 
 # GLOBAL parsing variables
 ALLOWED_FILE_EXTENSIONS = set(['gbk'])
+CLOSE12 = 12 # Number of base pair close to PAM
+EDIT_DISTANCE = 2
 
 
 def _logger_setup(logfile):
@@ -73,7 +76,7 @@ def _logger_setup(logfile):
         print("An error occurred setting up logging")
         raise e
 
-####################
+
 def allowed_file(filename):
     """ Check for the correct file type
     
@@ -112,7 +115,7 @@ def get_fastas(genbank, tempdir):
         print("Invalid file type, allowed file type is .gbk")
         
 
-############### MAPPING ###########################################
+
 def map_pam(tempdir, pamseq,threads,strand):
     """Runs seqkit locate to find the pam in given genome (FASTA)
 
@@ -152,8 +155,7 @@ def map_pam(tempdir, pamseq,threads,strand):
     except FileNotFoundError as f:
         raise f
 
-tempdir='/var/folders/52/rbrrfj5d369c35kd2xrktf3m0000gq/T/pamPredict_9z8641gp'
-######## Retrieving target sequence ################################
+
 def get_target(tempdir, mappingdata, targetlength, strand):
     """Given bedfile of PAM locations, function goes up or down and finds target
 
@@ -166,6 +168,8 @@ def get_target(tempdir, mappingdata, targetlength, strand):
     
     """
     target_dict = {}
+    # track keys so that any duplicated entry can be removed from the final dictionay
+    keys_list =[]
     infasta = SeqIO.read(os.path.join(tempdir, "out.fasta"), "fasta")
     infasta_complement = SeqIO.read(os.path.join(tempdir, "out_complement.fasta"), "fasta")
     bylines = mappingdata.splitlines()
@@ -183,6 +187,7 @@ def get_target(tempdir, mappingdata, targetlength, strand):
                 target_dict[target_seq]= {"seqid": seqid, "target_sp": target_sp,
                 "target_ep": pam_start_ps, "pam_seq": pam_seq,
                 "pam_start_ps": pam_start_ps, "strand": strand}
+            keys_list.append(target_seq)
     if strand == "-":
         for entry in bylines:
             tline = entry.split()
@@ -197,8 +202,12 @@ def get_target(tempdir, mappingdata, targetlength, strand):
                 target_dict[target_seq]={"seqid": seqid, "target_sp": pam_end_ps,
                 "target_ep": target_ep, "pam_seq": pam_seq,
                 "pam_start_ps": pam_start_ps, "strand": strand}
-    return target_dict
-############### Means to filter target sequences ############
+            keys_list.append(target_seq)
+    remove_target = [k for k, v in Counter(keys_list).items() if v > 1] # list of keys with more than one observation
+    target_dict2 = {k: v for k, v in target_dict.items() if k not in remove_target} # although dict over writes on non-unique key, but we want to complete remove such observation
+    return target_dict2
+
+
 # parse key then create 12 bp and remanining part, also take care of strand specifictity
 def parse_target(targetdict,strand):
     """Given a dictionary of target sequence, parse target sequence into two parts:
@@ -212,20 +221,26 @@ def parse_target(targetdict,strand):
         
     """
     parse_target_dict={}
+    # track keys so that any duplicated entry can be removed from the final dictionay
+    keys_list =[]
     for items in targetdict.items():
         if items[1]['strand']=="+":
-            close12 = items[0][-12:]
-            remainingseq = items[0][:-12]
+            close12 = items[0][-CLOSE12:]
+            remainingseq = items[0][:-CLOSE12]
             items[1]['target'] = items[0] # move target sequence as value
             items[1]['remainingseq'] = remainingseq
             parse_target_dict[close12] = items[1] ## will retain only target with unique close12 bp
+            keys_list.append(close12)
         if strand == "-":
-            close12 = target_key[:12]
-            remainingseq = target_key[12:]
+            close12 = target_key[:CLOSE12]
+            remainingseq = target_key[CLOSE12:]
             items[1]['target'] = items[0]
             items[1]['remainingseq'] = remainingseq
             parse_target_dict[close12] = items[1]
-    return parse_target_dict
+            keys_list.append(close12)
+    remove_target = [k for k, v in Counter(keys_list).items() if v > 1]
+    parse_target_dict2 = {k: v for k, v in parse_target_dict.items() if k not in remove_target}
+    return parse_target_dict2
 
 #nms lib - create a index with remainingseq
 def create_index(strings):
@@ -257,11 +272,10 @@ def filter_parse_target(parse_dict):
     # initialize a new index
     ref_index = create_index(list(parse_dict.keys()))
     for keys, value in parse_dict.items():
-        ids, distances = ref_index.knnQuery(keys, k=100) ## k =number of k nearest neighbours (knn)
-        check_values=[0,1,2] ## Levenshtein Distance greater than 2 is selected, less than 2 is considered as too similar -- maximizing specificity of target
-        check = np.isin(distances, value) # if any element of value in the array, then True else False
-        sum_check = np.sum(check) # If non present, all are false, hence sum == zero, else sum >=1
-        if sum_check == 0 :
+        ids, distances = ref_index.knnQuery(keys, k=1) ## k =number of k nearest neighbours (knn)
+        check_values= list(range(0, EDIT_DISTANCE + 1)) ## Levenshtein Distance greater than 2 is selected, less than or equal to 2 is considered as too similar -- maximizing specificity of target
+        # here distance is sort in ascending order
+        if distances not in check_values:
             filter_parse_dict[keys] = value
     return filter_parse_dict
 
@@ -275,29 +289,28 @@ def reformat_parse_target_for_pybed(filterpasrsedict):
     """
     dict_to_pd = pd.DataFrame.from_dict(filterpasrsedict,orient='index')
     dict_to_pd.reset_index(inplace=True) # reindex
-    dict_to_pd_reorder=dict_to_pd[['seqid', 'target_sp', 'target_ep', 'pam_seq', 'pam_start_ps','strand', 'target', 'remainingseq','index']] ## pybed need first column to be seqid/chromosome, next column start and thired column as end position
-    dict_to_pd_reorder.columns = ['seqid', 'target_sp', 'target_ep', 'pam_seq', 'pam_start_ps',
-    'strand', 'target', 'remainingseq','close12'] # rename index to close12
+    # pybed need first column to be seqid/chromosome, next column start and thired column as end position
+    dict_to_pd_reorder=dict_to_pd[['seqid', 'target_sp', 'target_ep', 'pam_seq', 'pam_start_ps','strand', 'target', 'remainingseq','index']]
+    # rename index to close12
+    dict_to_pd_reorder.columns = ['seqid', 'target_sp', 'target_ep', 'pam_seq', 'pam_start_ps','strand', 'target', 'remainingseq','close12']
+    # pybed takes tab separated file with no header, plus first three column has to be as above
     dict_to_pd_reorder_tab = dict_to_pd_reorder.to_csv(index=False,sep='\t',header=False)
     return dict_to_pd_reorder_tab
 
 
-###################
-# filter_parse_dict need to convert to list with appropriate order then can be passed for pybed tools
-
-#############
-def get_cds(genebank_record):
+def get_cds(genebank):
     """Return a list of CDS's for a genbank file
 
     Args:
-        genbank_record (obj): Biopython genbank object
+        genbank (genebank): Genbank file to process
 
 
     Returns:
         (list): List of CDS
     """
     cds_list = []
-    for cds_record in genebank_record:
+    genebank_file = SeqIO.parse(genebank,"genbank")
+    for cds_record in genebank_file:
         for cds_feature in cds_record.features:
             if cds_feature.type == "CDS":
                 start = cds_feature.location.start.position
@@ -306,25 +319,13 @@ def get_cds(genebank_record):
                 des = cds_record.description
                 type = cds_feature.type
                 try:
-                    geneID = cds_feature.qualifiers['db_xref'][0]
-                except KeyError:
-                    geneID = None
-                try:
                     locus_tag = cds_feature.qualifiers['locus_tag'][0]
                 except KeyError:
                     locus_tag = None
                 try:
-                    amino = cds_feature.qualifiers['translation'][0]
-                except KeyError:
-                    amino = None
-                try:
                     product = cds_feature.qualifiers['product'][0]
                 except KeyError:
                     product = None
-                try:
-                    other = cds_feature.qualifiers['note'][0]
-                except KeyError:
-                    other = None
                 if cds_feature.strand < 0:
                     strand = "-"
                 else:
@@ -337,17 +338,23 @@ def get_cds(genebank_record):
 
 
 def get_gene(genebank_record):
+    """Return a list of genes for a genbank file
+
+    Args:
+        genbank (genebank): Genbank file to process
+
+
+    Returns:
+        (list): List of genes
+    """
     gene_list = []
-    for gene_record in genebank_record:
+    genebank_file = SeqIO.parse(genebank,"genbank")
+    for gene_record in genebank_file:
         for gene_feature in gene_record.features:
             if gene_feature.type == 'gene':
                 start = gene_feature.location.start.position
                 stop = gene_feature.location.end.position
                 accession = gene_record.id
-                try:
-                    des = gene_feature.description
-                except AttributeError:
-                    des = None
                 type = gene_feature.type
                 try:
                     geneID = gene_feature.qualifiers['db_xref'][0]
@@ -371,6 +378,15 @@ def get_gene(genebank_record):
     return gene_list
 
 def merge_cds_gene(cdslist, genelist, tempdir):
+    """Return a merged cds and gene list based on locus_tag
+
+    Args:
+        (list): List of CDS
+        (list): List of genes
+        
+    Returns:
+        (dataframe): A DataFrame with mergred gene and cds based on locus_tag
+    """
     cds_df = pd.DataFrame(cdslist)
     gene_df = pd.DataFrame(genelist)
     merge_df = pd.merge(gene_df, cds_df,  on=["locus_tag"], how='outer')
@@ -380,20 +396,47 @@ def merge_cds_gene(cdslist, genelist, tempdir):
 # ######### pybedtools ########
 
 def pybed_downstream(tempdir, mapfile_from_pam):
+    """Adds downstream information to the given target sequences and mapping information
+
+    Args:
+        (tsv): mapping file with target sequence
+        
+    Returns:
+        (tsv): A file with target sequences, mapping information, and downstream information
+    """
     featurefile = os.path.join(tempdir, "features.txt")
     mapbed = BedTool(mapfile_from_pam.splitlines())
-    downstream = mapbed .closest(featurefile , d=True, fd=True, D="a", t="first")
+    # -d reports distance , fb reports first downsteam feature from feature file in reference to information provided in mappiing file(D=a)
+    downstream = mapbed.closest(featurefile , d=True, fd=True, D="a", t="first")
     return downstream
 
 
 def pybed_upstream(tempdir,mapfile_from_pam):
+    """Adds upstream information to the given target sequences and mapping information
+
+    Args:
+        (tsv): mapping file with target sequence
+        
+    Returns:
+        (tsv): A file with target sequences, mapping information, and upstream information
+    """
     featurefile = os.path.join(tempdir, "features.txt")
     mapbed = BedTool(mapfile_from_pam.splitlines())
+    # -d reports the distance, t reports the first matched element, id = igrnore downstream, D indicates with reference to or in reference to
     upstream = mapbed.closest(featurefile , d=True, id=True, D="a", t="first")
     return upstream
 
 
 def merge_downstream_upstream(downsfile,upsfile,tempdir):
+    """Return a merged file
+
+    Args:
+        (tsv): A file with target sequences, mapping information, and upstream information
+        (tsv): A file with target sequences, mapping information, and downstream information
+        
+    Returns:
+        (dataframe): A DataFrame with mergred upstream information and downstream information for a target sequence
+    """
     n = downsfile.to_dataframe().shape[1]
     rownames = list(string.ascii_uppercase[0:n])
     downstream_df = downsfile.to_dataframe(names=rownames,low_memory=False)
@@ -434,7 +477,7 @@ def main(args=None):
         logging.info("Mapping pam to the genome")
         mapfile = map_pam(tempdir=tempdir, pamseq=args.pamseq, threads=args.threads, strand=args.strand)
 
-        #Retrieving target sequence
+        # Retrieving target sequence
         logging.info("Retrieving target sequence for matching PAM : %s", tempdir)
         targetdict = get_target(tempdir=tempdir, mappingdata=mapfile, targetlength=args.targetlength, strand=args.strand)
         
@@ -443,14 +486,13 @@ def main(args=None):
         parsetargetdict = parse_target(targetdict, strand=args.strand)
         
         # Calculate Levenshtein distance among remainingseq, and remove any remainingseq that are similar
-        logging.info("Filtering parse target sequence based on Levenshtein distance using NMSLIB index, with knn=100")
+        logging.info("Filtering parse target sequence based on Levenshtein distance using NMSLIB index, with knn=1")
         filterparsetargetdict = filter_parse_target(parsetargetdict)
         
-        ## Formatiing filter parse target sequnece as needed for pybed tools
+        # Formatiing filter parse target sequnece as needed for pybed tools
         logging.info("Formatiing filter parse target sequnece as needed for pybed tools")
         tabfile_for_pybed= reformat_parse_target_for_pybed(filterparsetargetdict)
-
-
+        
         # get cds list
         logging.info("Retrieving CDS information")
         cdslist = get_cds(SeqIO.parse(args.gbkfile, "genbank"))
