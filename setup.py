dedc6226bbf2b033c9ff2af52ca5556421fa0040
ravin.poudel <admin@admins-Air.hsd1.ma.comcast.net>
2021-04-14 17:06:05 -0400

    modify setup.py and remove requirement.txt

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ import versioneer
 requirements = [
     # package requirements go here
     'biopython==1.76',
-    'numpy >=1.11',
+    'numpy >=1.13',
     'pybedtools',
     'nmslib>=2.0.4',
     'pandas>=1.0.0',
@@ -27,22 +27,39 @@ setup(
     name='guidemaker',
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    packages=['guidemaker'],
-    license='CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
     description='GuideMAker: globally design gRNAs for any CRISPR-Cas system in any small genome',
-    long_description=open('README.md').read(),
-    classifiers=['Topic :: Scientific/Engineering :: Bio-Informatics',
-                 'Programming Language :: Python :: 3.7',
-                 'Programming Language :: Python :: 3.8',
-                 'Development Status :: 3 - Alpha'],
-    keywords='CRISPR-Cas',
-    url='http://tinyecology.com',
-    test_suite='pytest',
+    license='CC0 1.0 Universal (CC0 1.0) Public Domain Dedication',
     author='Adam Rivers',
     author_email='adam.rivers@usda.gov',
+    url='http://tinyecology.com',
+    long_description=open('README.md').read(),
+    packages=['guidemaker'],
+    entry_points={
+        'console_scripts': [
+            'guidemaker=guidemaker.cli:main'
+        ]
+    },
     install_requires=requirements,
+    python_requires='>=3.6',
     test_suite='pytest',
     tests_require=['pytest'],
+    keywords='CRISPR-Cas',
     include_package_data=True,
-    entry_points={'console_scripts':['guidemaker = guidemaker.cli:main',]},
-    zip_safe=False)
+    zip_safe=False,
+    classifiers=[
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Development Status :: 3 - Alpha'
+    ]
+)
+
+
+
+    
+    
+    
+    
+    
+    
+  
+    
